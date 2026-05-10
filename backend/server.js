3e0c0916af8bfd2e5ba180911935ded124b56b9c
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2026-05-10 16:44:45 +0530

    fix some type before deploying

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.get('/api/health', (req, res) => {
 app.use(express.static(path.join(__dirname, 'frontend/dist')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
 })
 
 
